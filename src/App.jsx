f07215803873cccec1ea7bb865f555fd74e7e1b9
Abdul Wahab <abdulwahab020070@gmail.com>
2023-10-17 20:41:31 +0500

    Sticky NavBar Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
 
   return (
     <>
-    <div className='Main'>
-      <SideBar/>
-      <MainContent/>
-    </div>
+      <div className='Main'>
+        <SideBar />
+        <MainContent />
+      </div>
     </>
   )
 }
