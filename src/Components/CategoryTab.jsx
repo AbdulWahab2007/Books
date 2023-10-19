38e9a03a09f6456332b8db36f1d678d214bad23e
Abdul Wahab <abdulwahab020070@gmail.com>
2023-10-19 20:59:14 +0500

    Books from API & PrevNext buttons working

---
+++ src/Components/CategoryTab.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react"
+import React from 'react'
+import Card from './Card'
+
+export default function CategoryTab(props) {
+
+    const [items, setItems] = useState([])
+    const [index, setIndex] = useState(0)
+    useEffect(() => {
+        LoadingCards();
+    }, [])
+
+    let url = `https://www.googleapis.com/books/v1/volumes?q=subject:${props.CategoryName}&startIndex=${index}&maxResults=6`
+    const LoadingCards = async () => {
+        let Data = await fetch(url)
+        let ParsedData = await Data.json()
+        setItems(ParsedData.items);
+    }
+    const HandleNextClick = async () => {
+        let url = `https://www.googleapis.com/books/v1/volumes?q=subject:${props.CategoryName}&startIndex=${index + 6}&maxResults=6`
+        setIndex(index + 6);
+        let Data = await fetch(url)
+        let ParsedData = await Data.json()
+        setItems(ParsedData.items);
+    }
+    const HandlePrevClick = async () => {
+        if (index >= 6) {
+            let url = `https://www.googleapis.com/books/v1/volumes?q=subject:${props.CategoryName}&startIndex=${index - 6}&maxResults=6`
+            setIndex(index - 6);
+            let Data = await fetch(url)
+            let ParsedData = await Data.json()
+            setItems(ParsedData.items);
+        }
+    }   
+    return (
+        <>
+
+
+            <div className="Category">
+                <div className='CategoryTop'>
+                    <h2>{props.CategoryName}</h2>
+                    <div className="PrevNext">
+                        <button id="prevbutton" onClick={HandlePrevClick}><span className="material-symbols-outlined">
+                            arrow_back_ios
+                        </span></button>
+                        <button onClick={HandleNextClick}><span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span></button>
+                    </div>
+                </div>
+                <div className='CategoryBooks'>
+
+                    {items.map((element, index) => {
+                        return <div key={index}>
+                            <Card title={element.volumeInfo.title} author={element.volumeInfo.authors} category={element.volumeInfo.categories} img={element.volumeInfo.imageLinks.thumbnail} source={element.volumeInfo.infoLink} />
+                        </div>
+
+                    })}
+
+                </div>
+            </div>
+
+        </>
+    )
+}
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,18 +1,8 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
+import CategoryTab from './CategoryTab'
 
 export default function MainContent() {
-  const [items, setItems] = useState([])
-  useEffect(() => {
-    LoadingCards();
-  }, [])
-
-  let url = "https://www.googleapis.com/books/v1/volumes?q=subject:fiction&startIndex=0&maxResults=6"
-  const LoadingCards = async ()=> {
-    let Data = await fetch(url)
-    let ParsedData = await Data.json()
-    setItems(ParsedData.items);
-  }
   return (
     <>
       <div className='Content'>
@@ -29,95 +19,11 @@ export default function MainContent() {
           <Card title="Talk Of Love" />
           <Card title="Talk Of Love" />
   </div>*/}
-        <div className="Category">
-          <div className='CategoryTop'>
-            <h2>Fiction</h2>
-            <div className="PrevNext">
-              <button id="fiction-prev" className="pagination prev"><span className="material-symbols-outlined">
-                arrow_back_ios
-              </span></button>
-              <button id="fiction-next" className="pagination next"><span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span></button>
-            </div>
-          </div>
-          <div className='CategoryBooks'>
+        <CategoryTab CategoryName="fiction"/>
+        <CategoryTab CategoryName="poetry"/>
+        <CategoryTab CategoryName="fantasy"/>
+        <CategoryTab CategoryName="romance"/>
 
-          {items.map((element, index) => {
-              return <div key={index}>
-                <Card title = {element.volumeInfo.title} author={element.volumeInfo.authors} category={element.volumeInfo.categories} img={element.volumeInfo.imageLinks.smallThumbnail} source={element.volumeInfo.infoLink} />
-              </div>
-
-            })}
-
-          </div>
-        </div>
-
-        <div className="Category">
-          <div className='CategoryTop'>
-            <h2>Poetry</h2>
-            <div className="PrevNext">
-              <button id="fiction-prev" className="pagination prev"><span className="material-symbols-outlined">
-                arrow_back_ios
-              </span></button>
-              <button id="fiction-next" className="pagination next"><span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span></button>
-            </div>
-          </div>
-          <div className='CategoryBooks'>
-            <Card img="https://books.google.com/books/content?id=KVGd-NabpW0C&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="The Plague" author="Albert Camus" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=b6nCAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Joseph Andrews" author="Henry Fielding" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=L88qEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Sherlock Holmes - The Valley Of Fear" author="Arthur Conana Doyle" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=TayGZxfYF_EC&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="Future Shock" author="Alvin Toffler" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=In8mDwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="How To Stop Time" author="Matt Haig" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=tigqEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="The Adventures Of Tom Sawyers" author="Mark Twain" category="Fiction" />
-          </div>
-        </div>
-
-        <div className="Category">
-          <div className='CategoryTop'>
-            <h2>Fantasy</h2>
-            <div className="PrevNext">
-              <button id="fiction-prev" className="pagination prev"><span className="material-symbols-outlined">
-                arrow_back_ios
-              </span></button>
-              <button id="fiction-next" className="pagination next"><span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span></button>
-            </div>
-          </div>
-          <div className='CategoryBooks'>
-            <Card img="https://books.google.com/books/content?id=KVGd-NabpW0C&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="The Plague" author="Albert Camus" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=b6nCAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Joseph Andrews" author="Henry Fielding" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=L88qEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Sherlock Holmes - The Valley Of Fear" author="Arthur Conana Doyle" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=TayGZxfYF_EC&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="Future Shock" author="Alvin Toffler" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=In8mDwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="How To Stop Time" author="Matt Haig" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=tigqEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="The Adventures Of Tom Sawyers" author="Mark Twain" category="Fiction" />
-          </div>
-        </div>
-
-        <div className="Category">
-          <div className='CategoryTop'>
-            <h2>Romance</h2>
-            <div className="PrevNext">
-              <button id="fiction-prev" className="pagination prev"><span className="material-symbols-outlined">
-                arrow_back_ios
-              </span></button>
-              <button id="fiction-next" className="pagination next"><span className="material-symbols-outlined">
-                arrow_forward_ios
-              </span></button>
-            </div>
-          </div>
-          <div className='CategoryBooks'>
-            <Card img="https://books.google.com/books/content?id=KVGd-NabpW0C&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="The Plague" author="Albert Camus" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=b6nCAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Joseph Andrews" author="Henry Fielding" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=L88qEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="Sherlock Holmes - The Valley Of Fear" author="Arthur Conana Doyle" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=TayGZxfYF_EC&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="Future Shock" author="Alvin Toffler" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=In8mDwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api" title="How To Stop Time" author="Matt Haig" category="Fiction" />
-            <Card img="https://books.google.com/books/content?id=tigqEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" title="The Adventures Of Tom Sawyers" author="Mark Twain" category="Fiction" />
-          </div>
-        </div>
         <a href="https://github.com/AbdulWahab2007/Books" target='_blank' className='GitHubLink'><img src="https://liyasthomas.github.io/books/icons/github.svg" alt="" /></a>
       </div>
     </>
